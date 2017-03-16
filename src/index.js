162e884bad978b199b26ac3abd8542e34fc08da8
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-03-16 14:57:48 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import "./styles.scss";
+import component from "./component";
+
+
+//CARTOGRAM TOOL
+const Cartogram = Vizabi.Tool.extend("Cartogram", {
+
+  /**
+   * Initialized the tool
+   * @param {Object} placeholder Placeholder element for the tool
+   * @param {Object} external_model Model as given by the external page
+   */
+  init(placeholder, external_model) {
+
+    this.name = "cartogram";
+
+    //specifying components
+    this.components = [{
+      component,
+      placeholder: ".vzb-tool-viz",
+      model: ["state.time", "state.entities", "state.marker", "locale", "ui"] //pass models to component
+    }, {
+      component: Vizabi.Component.get("timeslider"),
+      placeholder: ".vzb-tool-timeslider",
+      model: ["state.time", "state.entities", "state.marker", "ui"]
+    }, {
+      component: Vizabi.Component.get("dialogs"),
+      placeholder: ".vzb-tool-dialogs",
+      model: ["state", "ui", "locale"]
+    }, {
+      component: Vizabi.Component.get("buttonlist"),
+      placeholder: ".vzb-tool-buttonlist",
+      model: ["state", "ui", "locale"]
+    }, {
+      component: Vizabi.Component.get("treemenu"),
+      placeholder: ".vzb-tool-treemenu",
+      model: ["state.marker", "locale"]
+    }, {
+      component: Vizabi.Component.get("datawarning"),
+      placeholder: ".vzb-tool-datawarning",
+      model: ["locale"]
+    }, {
+      component: Vizabi.Component.get("datanotes"),
+      placeholder: ".vzb-tool-datanotes",
+      model: ["state.marker", "locale"]
+    }
+    ];
+
+    //constructor is the same as any tool
+    this._super(placeholder, external_model);
+  },
+
+  default_model: {
+    state: { },
+    ui: {
+      chart: {
+        labels: {
+          dragging: true
+        },
+        lockNonSelected: 0,
+        lockActive: 0,
+        sizeSelectorActive: 0
+      },
+      datawarning: {
+        doubtDomain: [],
+        doubtRange: []
+      },
+      presentation: false
+    }
+  }
+});
+
+export default Cartogram;
---
+++ webpack.external.js
@@ -0,0 +1,1 @@
+module.exports = require("vizabi-tool-bundler").bind(null, "CartogramChart", "cartogramchart", __dirname);
